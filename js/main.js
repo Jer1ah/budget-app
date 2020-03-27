9ce88762896a3bbac43bc825fd27052c868c9de7
Jeriah Bowers <jeriah97bowers@yahoo.com>
2020-03-26 23:42:12 -0400

    finishing up the logic

--- js/main.js
+++ js/main.js
@@ -36,6 +36,59 @@ const createListItem = (plusMinus, title, total) => {
     list.appendChild(listItem);
 };
 
+//Adding functionality for main total
+const mainTotal = document.getElementsByTagName("H1")[0];
+const incomeTotal = document.getElementsByClassName("plusRight")[0];
+const expensesTotal = document.getElementsByClassName("minusRight")[0];
+const expenseList = [...document.getElementsByClassName("spanMinus")];
+const incomeList = [...document.getElementsByClassName("spanAdd")];
+let total = 0;
+let plusTotal = 0;
+let minusTotal = 0;
+
+//Removing the commas and peroid in the strings and converting to numbers
+incomeList.forEach(number => {
+    let arr = number.innerHTML.split("");
+    arr.shift();
+    let str = arr.join("");
+    let num = parseFloat(str.replace(/,/g, ''));
+    plusTotal += num;
+});
+
+//Removing the commas and peroid in the strings and converting to numbers
+expenseList.forEach(number => {
+    let arr = number.innerHTML.split("");
+    arr.shift();
+    let str = arr.join("");
+    let num = parseFloat(str.replace(/,/g, ''));
+    minusTotal += num;
+});
+
+//Adding totals to the app
+incomeTotal.innerHTML = "+ " + (Number(plusTotal)).toLocaleString("en-US") + ".00";
+expensesTotal.innerHTML = "- " + (Number(minusTotal)).toLocaleString("en-US") + ".00";
+
 checkButton.addEventListener("click", () => {
     createListItem(plusMinusInput.value, descriptionInput.value, valueInput.value);
+    if( plusMinusInput.value === "income" ) {
+        plusTotal += Number(valueInput.value);
+        incomeTotal.innerHTML = "+ " + (Number(plusTotal)).toLocaleString("en-US") + ".00";
+    } else {
+        minusTotal += Number(valueInput.value);
+        expensesTotal.innerHTML = "- " + (Number(minusTotal)).toLocaleString("en-US") + ".00";
+    }
+    total = (plusTotal - minusTotal);
+    let str = String(total);
+    let arr = str.split("");
+    arr.shift();
+    let totalString = arr.join("");
+    console.log(typeof totalString);
+    if( plusTotal > minusTotal ) {
+        mainTotal.innerHTML = "+" + (Number(str)).toLocaleString("en-US") + ".00";
+    } else {
+        mainTotal.innerHTML = (Number(str)).toLocaleString("en-US") + ".00";
+    }
 });
+
+
+
